9dc9d81159fba675dcbd91f1f9364826f0ec0d41
moshiour <moshiour0027@gmail.com>
2020-02-15 01:39:33 +0600

    initialized

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const getDefaultTheme = () => {
+        const isDarkModeEnabled = JSON.parse(localStorage.getItem("is-dark-mode-enabled"));
+        return isDarkModeEnabled || false;
+    };
+
+    const [darkTheme, setDarkTheme] = React.useState(getDefaultTheme());
+
+    React.useEffect(() => {
+        localStorage.setItem("is-dark-mode-enabled", JSON.stringify(darkTheme));
+    }, [darkTheme]);
+
+
+    return (
+        <div className={darkTheme ? "techy-dark-theme" : "techy-light-theme"}>
+            <nav>
+                <div className="button-container">
+                    <button onClick={() => setDarkTheme(prevTheme => !prevTheme)}>
+                        Change Theme
+                    </button>
+                </div>
+            </nav>
+            <div className="content">
+                <h1>{darkTheme ? "Dark Mode" : "Light Mode"}</h1>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
